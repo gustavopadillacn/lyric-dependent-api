13c63b289ad9b2c297af524e33f7bcdc821f3d15
gustavopadillacn <gustavo@clearnexus.com>
2025-06-16 12:46:08 -0600

    Update create-dependent.js

--- api/create-dependent.js
+++ api/create-dependent.js
@@ -1,1 +1,10 @@
+module.exports = async (req, res) => {
+  const body = req.body;
 
+  console.log("📩 Datos recibidos del formulario:", body);
+
+  return res.status(200).json({
+    success: true,
+    message: "✅ Dependent received (mock response from Vercel)."
+  });
+};
